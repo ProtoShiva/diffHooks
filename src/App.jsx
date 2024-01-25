64b308acaedd8f11bd3a8b9db4cad7d25082152f
Himanshu <hgusain088@gmail.com>
2024-01-25 12:38:07 +0530

    intital commit and learing all hooks in this file apart from usestate

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import EffectTutorial from "./EffectTutorial.jsx"
+import ReducerTutorial from "./ReducerTutorial"
+function App() {
+  return (
+    <>
+      {/* <ReducerTutorial /> */}
+      <EffectTutorial />
+    </>
+  )
+}
+
+export default App
